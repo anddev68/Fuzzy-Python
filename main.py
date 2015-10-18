8fd58fb1d26242e6dbfc8232c9423d67f1f19227
anddev68 <chk392013@gmail.com>
2015-10-19 08:05:33 +0900

    add fuzzy_nomal

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
+import copy
 import numpy as np
+import math
 
-##	Initialize
+##	Initialize Parameter
 
 #	qvalue
 q = 1.1
@@ -10,10 +12,14 @@ N = 20
 #	Number of center of cluster,v(Integer)
 C = 2
 #	Initialize temperature
-T = 2000
+Thigh = T = 20
 #	Continue Conditions
 e1 = 0.01
 e2 = 0.01
+#	param for cooling schedule
+Cd = 2.0
+#	Demention of data
+D = 2
 
 
 ##	Print Method
@@ -22,19 +28,22 @@ def printArray(array,name):
 	for v in array:
 		print name + "[" + str(i) + "]=" + str(v)
 		i+=1
-		
-##	Calculate u[i][k]
-def calculateU(){
-	
-}
+			
+			
+##	Calculate vbefore[i] - vafter[i]
+##	@return
+##		sum distance that v moved
+def sumDistance(before,after):
+	total = 0
+	for i in range(C):
+		t = after[i] - before[i]
+		dist = np.linalg.norm(t)	# calculate a distance v moved
+		total+=dist
+	return total	
 
+				
 
-
-
-
-
-		
-
+##	Initalize
 
 #	Initialize center of cluster,v[i] as random
 #	demation p = 2D
@@ -43,6 +52,8 @@ v = []
 while i<C:
 	v.append(np.array([random.random(),random.random()]))
 	i+=1
+v1 = copy.deepcopy(v)
+v2 = copy.deepcopy(v)
 #	Initalize set of data,x[k] as random
 #	demation p = 2D
 x = []
@@ -52,96 +63,94 @@ while k<N:
 	k+=1
 #	Initalize membership function,u[i][k]
 u = [ [0 for k in range(N)] for i in range(C)]
-
 #	Print
-print "---------- DATA SET ----------"
-printArray(x,"x")
-print ""
-printArray(v,"v")
+#print "---------- DATA SET ----------"
+#printArray(x,"x")
+#print ""
+#printArray(v,"v")
+
+#	update q value
+q = (Thigh+0.01) / T 
 
 ##	mainLoop	
 ##
 ##	Continue Conditions:
 ##		Not convergence
-
-
-
-
-
-pre_temperature = temperature	# temperature before changing 
-
-
-#	calculate u[i][k]
-beta = 1.0/T
-k = 0
-while k<N:
-	denominator = 0
-	j = 0
-	while j<C:
-		t = x[k] - v[j]
-		djk = np.linalg.norm(t)	# calculate djk with norm method
-		denominator += (1.0-beta*(1-q)*djk)**(1/(1-q))
-		j+=1
-	i = 0
-	while i<C:
-		t = x[k] - v[i]
-		dik = np.linalg.norm(t)
-		numerator = (1-beta*(1-q)*dik)**(1/(1-q))
-		u[i][k] = numerator / denominator
-		i+=1
-	k+=1
-
-
-
-#	calculate v[i]
-i = 0
-while i<C:
-	denominator = 0
-	numerator = np.array([0,0])
+m = 0	#	loop counter
+while m < 100:
+	#	inc loop counter
+	m+=1
+	
+	printArray(v,"v")
+	
+	#	calculate u[i][k]
+	beta = 1.0/T
 	k = 0
 	while k<N:
-		denominator += u[i][k] ** q
-		numerator += denominator * x[k]
-		k+=1
-	v[i] = numerator / denominator
-	i+=1
+		denominator = 0.0
+		j = 0
+		while j<C:
+			t = x[k] - v[j]
+			djk = np.linalg.norm(t)	# calculate djk with norm method
+			denominator += (1.0-beta*(1.0-q)*djk)**(1.0/(1.0-q))
+			j+=1
+		i = 0
+		while i<C:
+			t = x[k] - v[i]
+			dik = np.linalg.norm(t)
+			numerator = (1.0-beta*(1.0-q)*dik)**(1.0/(1.0-q))
+			#	This line is changing u[i][k]
+			u[i][k] = numerator / denominator
+			i+=1
+		k+=1	
+		
 
-
-#	d1 is a distance that center of cluster,v moved at same temperature
-d1 = 
-#	d2 is a distance that center of cluster,v moved at before temperature
-d2 = 
-
-#	Check convergence
-#	The distance is so long at same temperature,
-#	do loop with same temperature
-if( e1 < d1 ){
-	 
-}
-
-#	Check convergence
-#	The distance is so long at before temperature,
-#	do loop, and cool tempareture
-if( e2 < d2){
+	#	calculate v[i]
+	i = 0
+	while i<C:
+		denominator = 0.0
+		numerator = np.array([0.0,0.0])
+		k = 0
+		while k<N:
+			denominator += u[i][k] ** q
+			numerator += x[k]*(u[i][k] ** q)
+			k+=1
+		v[i] = numerator  / denominator
+		i+=1
 	
-}
+	#	distance center of claster moved compare with same q value	
+	d1 = sumDistance(v,v1)
+	if e1 < d1:
+		# copy v1
+		v1 = copy.deepcopy(v)
+		# goto loop
+		continue
+	
+	#	with before q value
+	d2 = sumDistance(v,v2)
+	if e2 < d2:
+		
+		#	cool T
+		#	VFA
+		T = Thigh * math.exp(-Cd*(m**(1.0/D)))			
+		#	update q value
+		q = (Thigh+0.01) / T 
+		#	copy v2
+		v2 = copy.deepcopy(v)
+		#	goto loop
+		continue
 
 
+	#	result converged
+	break
+	
+	
+printArray(x,"x")
 
-#	calculate object function, J_tsallis
-J = 0
-J1 = 0	# first section of J function
-J2 = 0	# second section of J function
-ramuda = 1 / beta
-for k in range(N):
-	for i in range(C):
-		t = x[k] - v[i]
-		dik = np.linalg.norm(t)
-		J1 += u[i][k] ** q * dik
-		J2 += u[i][k] ** q * log(q) * u[i][k]
-J = J1 + J2*ramuda
-
-
+print u[0]
+		
+	
+printArray(v,"v")
 
 
 
