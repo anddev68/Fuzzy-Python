629f9a533035e50bfbe53ed68953841a7013df00
chrome <chk392013@gmail.com>
2015-11-18 09:30:25 +0900

    fix fcm

--- script/iris.py
+++ script/iris.py
@@ -8,169 +8,139 @@ import math
 import sys
 
 #	
-#	Irisのデータを用いてクラスタリングを行う
+#	--- Iris.py ---
+# Irisのデータを用いてtsaliisエントロピー正則化FCM法を試す
 #
+
+
+#
+# グローバル変数宣言
+#
+g_iris = ds.load_iris() # Irisのデータをロードする
+g_data = iris.data  # Irisの4次元データ(150個)
+g_target = iris.target  # 正解 [0,0,0,1....]
+
+
+# 標準偏差を表示する
+# print "std="+str(np.std(x))
 
 
 #
 #	各種パラメータの設定
 #
 C = 3	# v length
-N = 30	# x length
+N = 150	# x length
 P = 4	# Demention
 q = 2.5
 Thigh = 0.0000000001
 
 
 #
-#	Irisのデータを用いて初期データを生成
-#	[2:4] 花弁の長さと幅のみ
-# 	[1:3]
-iris = ds.load_iris()
-x = iris.data
-N = len(x)
-
+# エントリーポイント
 #
-#	Irisの正解データ
-#
-target = iris.target
 
-#
-#	乱数による初期データの生成
-#
-v = np.array( [ np.random.rand(P) for i in range(C) ]) # クラスタ中心
-u = np.zeros([C,N])
+# fcmで求める 
+result = fcm(g_data)
+predict = result[0]
+loop = result[1]
 
-#
-#	標準偏差の表示
-#
-print "std="+str(np.std(x))
+# 正解とpredictを表示
+print "target="
+np.savetxt(sys.stdout,g_target[None],fmt='%.0f',delimiter=' ')
 
+print "predict="
+np.savetxt(sys.stdout,predict[None],fmt='%.0f',delimiter=' ')
 
+#	正答率を表示
+score = accuracy_score(g_target,predict)
+print str(score*100) + "% (" + str(score*N) + "/" + str(N) + ")"
 
-##
-##	メインループ
-##
-loop = 0
-Jbefore = float("inf")
-while True:
-	loop+=1
-	
-	T = Thigh * math.exp (-2.0*loop**(1.0/P))
-	beta = 1.0 / T
-	
-	
-	#	Cal u[i][k]
-	for k in range(N):
-		
-		denominator = 0.0
-		for j in range(C):
-			djk = np.linalg.norm(v[j]-x[k])
-			denominator += (1.0-beta*(1.0-q)*djk)**(1.0/(1.0-q))
-		
-		for i in range(C):
-			dik = np.linalg.norm(v[i]-x[k])
-			u[i][k] = (1.0-beta*(1.0-q)*dik)**(1.0/(1.0-q)) / denominator
-		
-	
-	#	Cal v[i]
-	for i in range(C):
-		
-		#	cal denominator
-		denominator = 0.0
-		for k in range(N):
-			denominator += u[i][k]**q
-		
-		#	cal numerator
-		numerator = np.zeros(P)
-		for k in range(N):
-			numerator += (u[i][k] ** q)*x[k]
-		
-		#	cal v
-		num = numerator / denominator
-
-			
-		v[i] = num
-		
-	
-	#	Cal Jfcm, object function
-	Jfcm = 0.0
-	for k in range(N):
-		for i in range(C):
-			dik = np.linalg.norm(v[i]-x[k])
-			Jfcm += (u[i][k] ** q) * dik  	
-	
-		
-	#	Check continue condition
-	if Jbefore <= Jfcm:
-		break
-		
-	Jbefore = Jfcm
-
-
-	#	update Jbefore
-#
-#	vの表示
-#
-print "v="
-print v
-
-#
 #	ループ回数の表示
-#
 print "loop=" + str(loop)
 
 
-
-
-#	帰属するクラスタの番号を保存しておく配列
-#	targetと比較するために作成
-result = np.array( [ np.argmax(u[:,k]) for k in range(N) ] )
-
 #
-#	結果のインデックスがバラバラなので先頭を0にするように変更する
-#	先頭のデータは0へ変更
-#	最後尾のデータは2へ変更
-#	変更をすべてに適用
-first = result[0]
-last = result[N-1]
-for k in range(N):
-	if result[k] == first:
-		result[k] = 0
-	elif result[k] == last:
-		result[k] = 2
-	else:
-		result[k] = 1	
-
-print "target="
-np.savetxt(sys.stdout,target[None],fmt='%.0f',delimiter=' ')
-
-print "result="
-np.savetxt(sys.stdout,result[None],fmt='%.0f',delimiter=' ')
-
-
+# fuzzy_clustring_method
+# ファジィクラスタリングを実行する
 #
-#	正答率を表示
+# 1.クラスタ中心vをランダムに決定する
+# 2.帰属度関数uをゼロクリアする
+# 3.以下の操作を終了条件を満たすまで繰り返す
+# @param x データ集合
+# @param P データの次元数
+# @param N データ集合の個数
+# @param C クラスタ中心の数
+# @param Thigh 初期温度
+# @return [loop,predict]
+#   loop:ループ回数
+#   predict:クラスタリング結果
 #
-score = accuracy_score(target,result)
-print str(score*100) + "% (" + str(score*N) + "/" + str(N) + ")"
+def fcm(x,P,N,C,Thigh):
 
+  # クラスタ中心を初期化する
+  v = np.array( [ np.random.rand(P) for i in range(C) ]) 
+  
+  # 帰属度関数を初期化する
+  u = np.zeros([C,N])
+  
+  # ループ開始
+  loop = 0
+  update_temperature = 0
+  while True:
+    # ループを更新する
+    loop+=1
+    # 温度を更新する
+	  T = Thigh * math.exp (-2.0*loop**(1.0/P))  
+	  beta = 1.0 / T
+	  
+	  
+	  #	--- Cal u[i][k] ---
+	  # ここの部分はデバッグ済み。触らない。
+	  for k in range(N):
+		
+		  denominator = 0.0
+		  for j in range(C):
+			  djk = np.linalg.norm(v[j]-x[k])
+			  denominator += (1.0-beta*(1.0-q)*djk)**(1.0/(1.0-q))
+		
+		  for i in range(C):
+			  dik = np.linalg.norm(v[i]-x[k])
+			  u[i][k] = (1.0-beta*(1.0-q)*dik)**(1.0/(1.0-q)) / denominator
+	  
+	  #	--- Cal v[i] ---
+	  # ここの部分はデバッグ済み。触らない。
+	  for i in range(C):
+		
+		  #	cal denominator
+		  denominator = 0.0
+		  for k in range(N):
+			  denominator += u[i][k]**q
+		
+		  #	cal numerator
+		  numerator = np.zeros(P)
+		  for k in range(N):
+			  numerator += (u[i][k] ** q)*x[k]
+		
+		  #	cal v
+		  num = numerator / denominator
+		  v[i] = num
+		  
+    # 収束判定を行う  
+    break 
+  
+  
+  # クラスタリング結果を取得
+  predict = np.array( [ np.argmax(u[:,k]) for k in range(N) ] )
 
+  # ラベルの再割り振り
+  first = predict[0]
+  last = predict[N-1]
+  for k in range(N):
+	  if result[k] == first:
+		  result[k] = 0
+	  elif result[k] == last:
+		  result[k] = 2
+	  else:
+		  result[k] = 1	
 
-#
-#	グラフにプロットする
-#
-#colors = ['g','r','c','m','y','k','w']
-#plt.axis([0.0,1.0,0.0,1.0])
-#for i in range(C):
-#	plt.plot(x2[i],y2[i],"o",color=colors[i]) 
-	
-#plt.plot(v[:][:,1],v[:][:,0],"x",color='b') # クラスタ集合を二次元平面にプロットする
-#plt.show()
-	
-
-
-
-
-
-
+  return [predict,loop]
